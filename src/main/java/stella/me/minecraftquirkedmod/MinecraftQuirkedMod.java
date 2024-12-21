c6af86ee2aaf8a23f7198dc8bd50db385d509b76
PolarKookie <polarkookie23@gmail.com>
2024-12-20 21:54:17 -0500

    Error

--- src/main/java/stella/me/minecraftquirkedmod/MinecraftQuirkedMod.java
+++ src/main/java/stella/me/minecraftquirkedmod/MinecraftQuirkedMod.java
@@ -2,6 +2,10 @@ package stella.me.minecraftquirkedmod;
 
 import net.fabricmc.api.ModInitializer;
 
+import net.fabricmc.fabric.api.particle.v1.FabricParticleTypes;
+import net.minecraft.particle.DefaultParticleType;
+import net.minecraft.registry.Registry;
+import net.minecraft.util.Identifier;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -9,9 +13,10 @@ public class MinecraftQuirkedMod implements ModInitializer {
 	public static final String MOD_ID = "minecraftquirkedmod";
 	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
 
+	public static final DefaultParticleType OFA_LIGHTNING = FabricParticleTypes.simple();
+
 	@Override
 	public void onInitialize() {
-
-		LOGGER.info("Hello Fabric world!");
+		Registry.register(Registry.PARTICLE_TYPE, new Identifier("MOD_ID", "ofa_lightning"), OFA_LIGHTNING);
 	}
 }
