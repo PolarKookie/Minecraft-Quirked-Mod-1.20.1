65b836df87c35d33848c296e1078989be30f5159
PolarKookie <polarkookie23@gmail.com>
2024-12-20 20:16:46 -0500

    Initial commit

---
+++ src/main/java/stella/me/minecraftquirkedmod/MinecraftQuirkedModClient.java
@@ -0,0 +1,10 @@
+package stella.me.minecraftquirkedmod;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class MinecraftQuirkedModClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
